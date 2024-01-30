bbe65ae9d3583a05af3894be49cd43beaeaea2d2
Liang Zhang <psychelzh@outlook.com>
2024-01-30 17:02:19 +0800

    Separate feature selection and model building steps
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/cpm.R
+++ R/cpm.R
@@ -37,7 +37,10 @@
 #'   \item{folds}{The corresponding fold for each observation when used as test
 #'     group in cross-validation.}
 #'
-#'   \item{real}{The real behavior data. This is the same as the input `behav`.}
+#'   \item{real}{The real behavior data. This is the same as the input `behav`
+#'     if no confounding variables are used. If confounding variables are used,
+#'     this is the residual of `behav` after regressing out the confounding
+#'     variables.}
 #'
 #'   \item{pred}{The predicted behavior data, with each column corresponding to
 #'     a model, i.e., both edges, positive edges, negative edges, and the row
@@ -82,27 +85,31 @@ cpm <- function(conmat, behav, ...,
   if (is.null(kfolds)) kfolds <- length(behav)
   folds <- crossv_kfold(length(behav), kfolds)
   # pre-allocation
+  edges <- array(
+    dim = c(kfolds, dim(conmat)[2], length(networks)),
+    dimnames = list(NULL, NULL, networks)
+  )
   pred <- matrix(
     nrow = length(behav),
     ncol = length(includes),
     dimnames = list(names(behav), includes)
   )
-  edges <- array(
-    dim = c(kfolds, dim(conmat)[2], length(networks)),
-    dimnames = list(NULL, NULL, networks)
-  )
   for (fold in seq_len(kfolds)) {
     rows_train <- folds != fold
-    # train models
     conmat_train <- conmat[rows_train, , drop = FALSE]
     conmat_test <- conmat[!rows_train, , drop = FALSE]
     behav_train <- behav[rows_train]
-    result <- .cpm(
-      conmat_train, behav_train, conmat_test,
-      thresh_method, thresh_level, bias_correct
+    cur_edges <- select_edges(
+      conmat_train, behav_train,
+      method = thresh_method,
+      level = thresh_level
     )
-    pred[!rows_train, ] <- result$pred
-    edges[fold, , ] <- result$edges
+    cur_pred <- predict_cpm(
+      conmat_train, behav_train, conmat_test,
+      cur_edges, bias_correct
+    )
+    pred[!rows_train, ] <- cur_pred
+    edges[fold, , ] <- cur_edges
   }
   list(
     folds = folds,
@@ -112,52 +119,50 @@ cpm <- function(conmat, behav, ...,
   )
 }
 
-.cpm <- function(conmat_train, behav_train, conmat_test,
-                 thresh_method, thresh_level, bias_correct) {
+# helper functions
+predict_cpm <- function(conmat, behav, conmat_new, edges, bias_correct) {
   if (bias_correct) {
-    center <- colmeans(conmat_train)
-    scale <- colVars(conmat_train, std = TRUE)
-    conmat_train <- fscale(conmat_train, center, scale)
-    conmat_test <- fscale(conmat_test, center, scale)
+    center <- colmeans(conmat)
+    scale <- colVars(conmat, std = TRUE)
+    conmat <- fscale(conmat, center, scale)
+    conmat_new <- fscale(conmat_new, center, scale)
   }
-  edges <- select_edges(
-    conmat_train, behav_train,
-    method = thresh_method,
-    level = thresh_level
-  )
-  x_train <- allocate_design(dim(conmat_train)[1])
-  x_test <- allocate_design(dim(conmat_test)[1])
-  for (network in networks) {
-    x_train[, network] <- rowsums(
-      conmat_train[, edges[, network], drop = FALSE]
+  allocate_predictors <- function(nrow) {
+    matrix(
+      1,
+      nrow = nrow, ncol = length(networks) + 1,
+      dimnames = list(NULL, c("(Intercept)", networks))
     )
-    x_test[, network] <- rowsums(
-      conmat_test[, edges[, network], drop = FALSE]
+  }
+  x <- allocate_predictors(dim(conmat)[1])
+  x_new <- allocate_predictors(dim(conmat_new)[1])
+  for (network in networks) {
+    x[, network] <- rowsums(
+      conmat[, edges[, network], drop = FALSE]
+    )
+    x_new[, network] <- rowsums(
+      conmat_new[, edges[, network], drop = FALSE]
     )
   }
   pred <- matrix(
-    nrow = dim(conmat_test)[1],
+    nrow = dim(conmat_new)[1],
     ncol = length(includes),
     dimnames = list(NULL, includes)
   )
   for (include in includes) {
-    if (include != "both") {
-      cur_x_train <- x_train[, c("(Intercept)", include)]
-      cur_x_test <- x_test[, c("(Intercept)", include)]
+    if (include == "both") {
+      cur_x <- x
+      cur_x_new <- x_new
     } else {
-      cur_x_train <- x_train
-      cur_x_test <- x_test
+      cur_x <- x[, c("(Intercept)", include)]
+      cur_x_new <- x_new[, c("(Intercept)", include)]
     }
-    cur_model <- stats::.lm.fit(cur_x_train, behav_train)
-    pred[, include] <- cur_x_test %*% cur_model$coefficients
+    model <- stats::.lm.fit(cur_x, behav)
+    pred[, include] <- cur_x_new %*% model$coefficients
   }
-  list(
-    edges = edges,
-    pred = pred
-  )
+  pred
 }
 
-# helper functions
 regress_counfounds <- function(resp, confounds) {
   stats::.lm.fit(cbind(1, confounds), resp)$residuals
 }
@@ -189,13 +194,5 @@ select_edges <- function(conmat, behav, ...,
     c(r_mat >= r_crit[2], r_mat <= r_crit[1]),
     ncol = 2,
     dimnames = list(NULL, networks)
-  )
-}
-
-allocate_design <- function(nrow) {
-  matrix(
-    1,
-    nrow = nrow, ncol = length(networks) + 1,
-    dimnames = list(NULL, c("(Intercept)", networks))
   )
 }
