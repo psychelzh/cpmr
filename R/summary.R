4f31f74c8253f3dbb3dc5ae72f0ee0b56c268ae1
Liang Zhang <psychelzh@outlook.com>
2026-03-12 12:46:19 +0800

    refactor: apply low-risk architecture cleanups

--- R/summary.R
+++ R/summary.R
@@ -31,11 +31,16 @@ summary.cpm <- function(
   performance <- stats::cor(object$real, object$pred, method = method)
   # summary edge selection
   edges <- if (!is.null(object$edges)) {
+    kfolds <- if (!is.null(object$params$kfolds)) {
+      object$params$kfolds
+    } else {
+      length(unique(object$folds))
+    }
     edges_count <- object$edges
     if (length(dim(object$edges)) == 3) {
       edges_count <- apply(object$edges, 1:2, sum)
     }
-    edges_count > edge_level * length(unique(object$folds))
+    edges_count > edge_level * kfolds
   }
   structure(
     list(
