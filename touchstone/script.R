5bf2afe6de032555765d97144ef6c1975cabf708
Liang Zhang <psychelzh@outlook.com>
2026-03-22 18:33:32 +0800

    Add standardized resample benchmark

--- touchstone/script.R
+++ touchstone/script.R
@@ -99,6 +99,38 @@ fit_resamples_setup <- rlang::expr({
   }
 })
 
+fit_resamples_standardize_edges_setup <- rlang::expr({
+  set.seed(20260322)
+  conmat_res <- matrix(rnorm(240 * 12000), nrow = 240)
+  behav_res <- rowMeans(conmat_res[, 1:80, drop = FALSE]) + rnorm(240, sd = 0.5)
+
+  build_spec <- function() {
+    !!build_spec_standardized_expr
+  }
+
+  fit_spec <- build_spec()
+
+  fit_resamples_default <- function(spec) {
+    if ("cpm_selection_cor" %in% getNamespaceExports("cpmr")) {
+      cpmr::fit_resamples(
+        spec,
+        conmat = conmat_res,
+        behav = behav_res,
+        resamples = 8L,
+        return_edges = "none"
+      )
+    } else {
+      cpmr::fit_resamples(
+        spec,
+        conmat = conmat_res,
+        behav = behav_res,
+        kfolds = 8L,
+        return_edges = "none"
+      )
+    }
+  }
+})
+
 touchstone::branch_install(install_dependencies = TRUE)
 
 touchstone::benchmark_run(
@@ -126,6 +158,12 @@ touchstone::benchmark_run(
 touchstone::benchmark_run(
   expr_before_benchmark = !!fit_resamples_setup,
   fit_resamples_default = fit_resamples_default(fit_spec),
+  n = 3
+)
+
+touchstone::benchmark_run(
+  expr_before_benchmark = !!fit_resamples_standardize_edges_setup,
+  fit_resamples_standardize_edges = fit_resamples_default(fit_spec),
   n = 3
 )
 
